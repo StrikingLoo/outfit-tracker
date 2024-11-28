66a6cf41f80eaed6b53ab704452d841f7d4723a6
strikingLoo <lucianostrika44@gmail.com>
2024-11-28 02:55:40 -0800

    add filter/stats capability

--- app.js
+++ app.js
@@ -28,6 +28,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/stats', (req, res) => {
+    res.sendFile(path.join(__dirname, 'clothing-filter.html'));
+});
+
 // Read clothing options
 app.get('/api/clothing-options', async (req, res) => {
     try {
